4e5d02e19d7d571aee1f7ea440eef29d4ea622d3
Marius Årnes <marius.appa@gmail.com>
2020-03-20 17:01:27 +0100

    intiialized

---
+++ components/Sidebar.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <div class="column is-2 is-sidebar-menu is-hidden-mobile">
+  <aside class="menu">
+    <p class="menu-label">
+      General
+    </p>
+    <ul class="menu-list">
+      <li><nuxt-link to="/#">Dashboard</nuxt-link></li>
+      <li><nuxt-link to="/customers">Customers</nuxt-link></li>
+      <li><nuxt-link to="/products">Products</nuxt-link></li>
+    </ul>
+    <p class="menu-label">
+      Transactions
+    </p>
+    <ul class="menu-list">
+      <li><a>Payments</a></li>
+      <li><a>Orders</a></li>
+    </ul>
+  </aside>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Sidebar"
+    }
+</script>
+
+<style lang="scss" scoped>
+  @import '~/assets/variables/_variables.scss';
+  .is-sidebar-menu
+  {
+    background: $color--dark-shade;
+  li a {
+    color: $color--light-shades;
+  }
+  }
+
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <section class="section">
+    <div class="columns full-width is-multiline">
+        <nav class="navbar column is-full" role="navigation" aria-label="main navigation">
+          <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+              <a class="navbar-item">
+                Home
+              </a>
+
+              <a class="navbar-item">
+                Documentation
+              </a>
+
+              <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                  More
+                </a>
+
+                <div class="navbar-dropdown">
+                  <a class="navbar-item">
+                    About
+                  </a>
+                  <a class="navbar-item">
+                    Jobs
+                  </a>
+                  <a class="navbar-item">
+                    Contact
+                  </a>
+                  <hr class="navbar-divider">
+                  <a class="navbar-item">
+                    Report an issue
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <div class="buttons">
+                  <a class="button is-primary">
+                    <strong>Sign up</strong>
+                  </a>
+                  <a class="button is-light">
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+    <table class="table column is-full">
+      <thead>
+
+      </thead>
+      <tbody>
+      <tr>
+        <th>
+          hola
+        </th>
+        <td>amigo</td>
+      </tr>
+      </tbody>
+    </table>
+      </div>
+  </section>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
