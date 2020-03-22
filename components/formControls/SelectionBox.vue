f3d1a15fb41068c702f3cec45997aa9a70d4ded7
Marius Årnes <marius.appa@gmail.com>
2020-03-22 17:16:21 +0100

    Created custom SelectionBox.vue

---
+++ components/formControls/SelectionBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="select is-small">
+    <select   v-model="selected">
+      <option selected="selected"> Select dropdown</option>
+      <option v-for="(item,index) in collection" :key="index">{{item}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "SelectBox",
+      props: {
+        collection: {
+          type: Array,
+          required: true
+        }
+      },
+      data() {
+        return {
+          selected:null
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
